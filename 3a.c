823d7445321b8ba65b80a488a688996aec24642b
wifix <chinmayghadei333@gmail.com>
2024-12-16 23:38:26 +0530

    Create 3a.c

---
+++ 3a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+if (argc != 3) {
+fprintf(stderr, "Usage: %s <m1.txt> <k1.txt>\n", argv[0]);
+exit(EXIT_FAILURE);
+}
+if (rename(argv[1], argv[2]) == -1) {
+perror("mv");
+exit(EXIT_FAILURE);
+}
+printf("Moved '%s' to '%s'\n", argv[1], argv[2]);
+return 0;
+}
